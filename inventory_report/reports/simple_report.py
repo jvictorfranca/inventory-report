f10c4348969db2dbab830d30431ada855c2ab192
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2022-03-31 15:49:05 -0300

    comeca o projeto e resolve requisito 1

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,83 @@
+import datetime
+
+
+class SimpleReport():
+    @classmethod
+    def get_older_date(self, list):
+        all_dates = [
+            obj["data_de_fabricacao"]
+            for obj in list
+        ]
+
+        date = [datetime.datetime.strptime(ts, "%Y-%m-%d") for ts in all_dates]
+        date.sort()
+        sorted = [datetime.datetime.strftime(ts, "%Y-%m-%d") for ts in date]
+        return sorted[0]
+
+    @classmethod
+    def get_closer_date(self, list):
+        all_dates = [
+            obj["data_de_validade"]
+            for obj in list
+        ]
+
+        date = [datetime.datetime.strptime(ts, "%Y-%m-%d") for ts in all_dates]
+        min_date = min(date, key=lambda x: abs(x - datetime.datetime.today()))
+        min_string = datetime.datetime.strftime(min_date, "%Y-%m-%d")
+        return min_string
+
+    @classmethod
+    def get_enterprise_with_most_sales(self, list):
+        enterprises_list = [
+          obj["nome_da_empresa"]
+          for obj in list
+        ]
+
+        my_dict = {i: enterprises_list.count(i) for i in enterprises_list}
+        max_value = max(my_dict, key=my_dict.get)
+        return max_value
+
+    @classmethod
+    def generate(self, list):
+        data_antiga = self.get_older_date(list)
+        data_validade = self.get_closer_date(list)
+        nome_empresa = self.get_enterprise_with_most_sales(list)
+        stringToReturn = f"Data de fabricação mais antiga: {data_antiga}\n" \
+            f"Data de validade mais próxima: {data_validade}\n"\
+            f"Empresa com maior quantidade de "\
+            f"produtos estocados: {nome_empresa}\n"
+        print(stringToReturn)
+        return stringToReturn
+
+listObject  = [
+  {
+    "id": 1,
+    "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP, GERANIUM MACULATUM ROOT, SODIUM CHLORIDE, THUJA OCCIDENTALIS LEAFY TWIG, ZINC, and ECHINACEA ANGUSTIFOLIA",
+    "nome_da_empresa": "Forces of Nature",
+    "data_de_fabricacao": "2019-08-04",
+    "data_de_validade": "2023-02-09",
+    "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
+    "instrucoes_de_armazenamento": "in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices    phasellus"
+  },
+  {
+    "id": 1,
+    "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP, GERANIUM MACULATUM ROOT, SODIUM CHLORIDE, THUJA OCCIDENTALIS LEAFY TWIG, ZINC, and ECHINACEA ANGUSTIFOLIA",
+    "nome_da_empresa": "Test",
+    "data_de_fabricacao": "2020-07-04",
+    "data_de_validade": "2023-02-09",
+    "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
+    "instrucoes_de_armazenamento": "in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices    phasellus"
+  },
+  {
+    "id": 1,
+    "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP, GERANIUM MACULATUM ROOT, SODIUM CHLORIDE, THUJA OCCIDENTALIS LEAFY TWIG, ZINC, and ECHINACEA ANGUSTIFOLIA",
+    "nome_da_empresa": "Forces of Nature",
+    "data_de_fabricacao": "2015-08-10",
+    "data_de_validade": "2023-02-09",
+    "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
+    "instrucoes_de_armazenamento": "in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices    phasellus"
+  }
+
+]
+
+SimpleReport.generate(listObject)
