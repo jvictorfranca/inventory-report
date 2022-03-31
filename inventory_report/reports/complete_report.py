3fd03a5b76ab47c183f4c27b58d97b0e9957b938
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2022-03-31 16:23:12 -0300

    faz o requisito 2

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,31 @@
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+
+    @classmethod
+    def get_aditional_string(self, list):
+        initial_string = "Produtos estocados por empresa: \n"
+        final_string = initial_string
+        enterprises_list = [
+          obj["nome_da_empresa"]
+          for obj in list
+        ]
+        my_dict = {i: enterprises_list.count(i) for i in enterprises_list}
+        for key in my_dict:
+            final_string += f"- {key}: {my_dict[key]}\n"
+        return final_string
+
+    @classmethod
+    def generate(self, list):
+        data_antiga = self.get_older_date(list)
+        data_validade = self.get_closer_date(list)
+        nome_empresa = self.get_enterprise_with_most_sales(list)
+        aditional_string = self.get_aditional_string(list)
+        stringToReturn = f"Data de fabricação mais antiga: {data_antiga}\n" \
+            f"Data de validade mais próxima: {data_validade}\n"\
+            f"Empresa com maior quantidade de "\
+            f"produtos estocados: {nome_empresa}\n\n"\
+            f"{aditional_string}"
+        print(stringToReturn)
+        return stringToReturn
