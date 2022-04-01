b239b60b4f7843b83f40a79f3398540b98723e63
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2022-04-01 19:25:09 -0300

    corrige linter terceira vez

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,16 +1,17 @@
 from collections.abc import Iterable
-#from inventory_report.importer import csv_importer
 from inventory_report.inventory.inventory_iterator import InventoryIterator
 
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
+# from inventory_report.importer import csv_importer
+
 
 class InventoryRefactor(Iterable):
+
     def __init__(self, importer):
         self.importer = importer
         self.data = []
-
     def import_data(self, path, tipo):
         self.data += self.importer.import_data(path)
         if tipo == "simples":
