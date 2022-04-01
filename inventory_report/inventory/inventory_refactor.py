16d0fadc6e82a75cb7c206a573e8eb7268cb21f4
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2022-04-01 18:15:01 -0300

    faz o 7

---
+++ inventory_report/inventory/inventory_refactor.py
@@ -0,0 +1,34 @@
+from collections.abc import Iterable
+from inventory_report.importer import csv_importer
+from inventory_report.inventory.inventory_iterator import InventoryIterator
+
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class InventoryRefactor(Iterable):
+    def __init__(self, importer):
+        self.importer = importer
+        self.data = []
+
+    def import_data(self, path, tipo):
+        self.data += self.importer.import_data(path)
+        if tipo == "simples":
+            dataToReturn = SimpleReport.generate(self.data)
+            print(dataToReturn)
+        elif tipo == "completo":
+            dataToReturn = CompleteReport.generate(self.data)
+        return dataToReturn
+
+    def __iter__(self):
+        return InventoryIterator(self.data)
+
+
+inventory = InventoryRefactor(csv_importer.CsvImporter)
+inventory.import_data("inventory_report/data/inventory.csv", "simples")
+
+iterator = iter(inventory)
+first = next(iterator)
+print(first)
+second = next(iterator)
+print(second)
