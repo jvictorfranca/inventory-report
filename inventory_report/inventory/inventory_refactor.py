35e244c2fb69640882803b586d3c6ff6b1bf53f7
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2022-04-01 19:14:37 -0300

    faz o 8

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,5 +1,5 @@
 from collections.abc import Iterable
-
+from inventory_report.importer import csv_importer
 from inventory_report.inventory.inventory_iterator import InventoryIterator
 
 from inventory_report.reports.complete_report import CompleteReport
@@ -15,10 +15,19 @@ class InventoryRefactor(Iterable):
         self.data += self.importer.import_data(path)
         if tipo == "simples":
             dataToReturn = SimpleReport.generate(self.data)
-            print(dataToReturn)
         elif tipo == "completo":
             dataToReturn = CompleteReport.generate(self.data)
         return dataToReturn
 
     def __iter__(self):
         return InventoryIterator(self.data)
+
+
+# inventory = InventoryRefactor(csv_importer.CsvImporter)
+# inventory.import_data("inventory_report/data/inventory.csv", "simples")
+
+# iterator = iter(inventory)
+# first = next(iterator)
+# print(first)
+# second = next(iterator)
+# print(second)
--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,30 @@
+import sys
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
 def main():
-    pass
+
+    if len(sys.argv) < 3:
+        return print("Verifique os argumentos", file=sys.stderr)
+        
+
+    path = sys.argv[1]
+    type = sys.argv[2]
+
+    importer = CsvImporter
+    splitted_path = path.split('.')
+    extension = splitted_path[1]
+
+    if extension == 'json':
+        importer = JsonImporter
+
+    if extension == 'xml':
+        importer = XmlImporter
+    
+
+    instancia = InventoryRefactor(importer)
+    report = instancia.import_data(path, type)
+    print(report, end='')
