23457cfe536a9ec830f3f2fcb8b22f00ba7afa9c
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2022-04-01 19:16:51 -0300

    corrige linter

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -5,11 +5,10 @@ from inventory_report.importer.xml_importer import XmlImporter
 
 from inventory_report.inventory.inventory_refactor import InventoryRefactor
 
-def main():
 
+def main():
     if len(sys.argv) < 3:
         return print("Verifique os argumentos", file=sys.stderr)
-        
 
     path = sys.argv[1]
     type = sys.argv[2]
@@ -23,8 +22,7 @@ def main():
 
     if extension == 'xml':
         importer = XmlImporter
-    
-
+ 
     instancia = InventoryRefactor(importer)
     report = instancia.import_data(path, type)
     print(report, end='')
